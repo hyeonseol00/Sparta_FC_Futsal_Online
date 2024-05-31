a88cff632b41a7ecd72a04dca65f16709d00b67f
선재영 <wodud1892@naver.com>
2024-05-31 21:01:35 +0900

    insertPlayer

--- src/insertPlayerScript.js
+++ src/insertPlayerScript.js
@@ -1,28 +1,28 @@
 // insertPlayers.js
 // player DB에 선수 데이터들을 삽입하는 코드 (한번만 실행하기)
 
-import { PrismaClient } from '@prisma/client';
-import playersData from './playerData';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const insertPrisma = new PrismaClient();
+import { prisma } from './utils/prisma/index.js';
+import playersData from './playerData.js';
 
 async function insertPlayers() {
   try {
-    for (let i = 0; i < playersData.length; i++) 
-        {
-        const playerData = playersData[i];
+    for (let i = 0; i < playersData.length; i++) {
+      const playerData = playersData[i];
 
-        await insertPrisma.player.create({
-          data: {
-            playerName: playerData.playerName,
-            speed: playerData.speed,
-            goalDecision: playerData.goalDecision,
-            shootPower: playerData.shootPower,
-            defence: playerData.defence,
-            stamina: playerData.stamina,
-          }
+      await prisma.player.create({
+        data: {
+          playerName: playerData.player_name,
+          speed: playerData.speed,
+          goalDecision: playerData.goal_decision,
+          shootPower: playerData.shoot_power,
+          defence: playerData.defence,
+          stamina: playerData.stamina,
+        },
       });
-      console.log(`선수 ${playerData.playerName} 삽입이 완료 되었습니다.`);
+      console.log(`선수 ${playerData.player_name} 삽입이 완료 되었습니다.`);
     }
   } catch (error) {
     console.error('삽입 에러', error);
