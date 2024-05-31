7edc9b0ee85d49bf6f66625f28c7273bff8b11bf
hyeonseol00 <hyeonseol22@naver.com>
2024-05-31 15:51:17 +0900

    ✨ update : 게임 플레이 라우터 추가

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import dotEnv from 'dotenv';
+import GamePlayRouter from './routes/game-play.router.js';
 dotEnv.config();
 
 const app = express();
@@ -8,7 +9,7 @@ const PORT = 3000;
 
 app.use(express.json());
 app.use(cookieParser());
-// app.use('/api', []);
+app.use('/api', [GamePlayRouter]);
 
 app.listen(PORT, () => {
   console.log(PORT, '포트로 서버가 열렸어요!');
