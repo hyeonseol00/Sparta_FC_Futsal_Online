5048f029664bc25f6886a8e97308d5b55a24a383
hyeonseol00 <hyeonseol22@naver.com>
2024-05-31 11:52:50 +0900

    🩹 fix : 라우터 추가부분 임시 주석처리

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const PORT = 3000;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api', []);
+// app.use('/api', []);
 
 app.listen(PORT, () => {
   console.log(PORT, '포트로 서버가 열렸어요!');
