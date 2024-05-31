e2d4295d9dc26372002491454592bc2e99c899f8
hyeonseol00 <hyeonseol22@naver.com>
2024-05-31 11:40:38 +0900

    🩹 fix : 컨벤션에 맞게 프리티어 적용

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
-import dotEnv from "dotenv";
+import dotEnv from 'dotenv';
 dotEnv.config();
 
 const app = express();
@@ -10,7 +10,6 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/api', []);
 
-app.listen(PORT, () =>
-{
-	console.log(PORT, '포트로 서버가 열렸어요!');
+app.listen(PORT, () => {
+  console.log(PORT, '포트로 서버가 열렸어요!');
 });
