4299bb8bb4f7d557bf0d73727f52bf4e618d3cc4
hyeonseol00 <hyeonseol22@naver.com>
2024-05-31 11:27:05 +0900

    🎉 add : 프로젝트 최초 등록

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import dotEnv from "dotenv";
+dotEnv.config();
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use('/api', []);
+
+app.listen(PORT, () =>
+{
+	console.log(PORT, '포트로 서버가 열렸어요!');
+});
