cfb7544a0ac2a2e3f1cc50d739e80bfd12c791a5
hyeonseol00 <hyeonseol22@naver.com>
2024-06-03 11:02:28 +0900

    ♻️ refactor : 네이밍 통일

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import dotEnv from 'dotenv';
-import GamePlayRouter from './routes/game-play.router.js';
 import errorHandlerMiddleware from './middlewares/error-handler.middleware.js';
+import gamePlayRouter from './routes/game-play.router.js';
 import userRouter from './routes/user.router.js';
 import owningPlayerRouter from './routes/owning_player.router.js';
 dotEnv.config();
@@ -12,7 +12,7 @@ const PORT = 3000;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api', [GamePlayRouter, userRouter, owningPlayerRouter]);
+app.use('/api', [gamePlayRouter, userRouter, owningPlayerRouter]);
 app.use(errorHandlerMiddleware);
 
 app.listen(PORT, () => {
