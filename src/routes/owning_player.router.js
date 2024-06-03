a4c99ab03fb67777aa41351891557530d7538632
diddntjd99 <diddntjd99@gmail.com>
2024-06-03 12:09:45 +0900

    강화 기능 기획대로 구현

--- src/routes/owning_player.router.js
+++ src/routes/owning_player.router.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 //강화 확률
 const enhancementProbability = {
+  0: 1,
   1: 0.95, //95%
   2: 0.81, //81%
   3: 0.64, //64%
@@ -16,9 +17,10 @@ const enhancementProbability = {
   8: 0.04, //4%
   9: 0.02, //2%
 };
+const explodedProbability = 0.1; //10%
 
 // 강화 API
-router.post('/reinforce', authMiddleware, async (req, res, next) => {
+router.patch('/reinforce', authMiddleware, async (req, res, next) => {
   try {
     const { userId } = req.user;
     const { playerId, grade } = req.body;
@@ -27,6 +29,12 @@ router.post('/reinforce', authMiddleware, async (req, res, next) => {
       return res
         .status(400)
         .json({ message: '데이터를 올바르게 입력해주세요.' });
+    }
+
+    if (grade >= 10) {
+      return res
+        .status(400)
+        .json({ message: '강화는 최대 10카 까지만 가능합니다.' });
     }
 
     const owningPlayer = await prisma.owningPlayer.findFirst({
@@ -40,28 +48,90 @@ router.post('/reinforce', authMiddleware, async (req, res, next) => {
       return res.status(401).json({ message: '갖고 있지 않은 선수입니다.' });
     }
 
-    if (owningPlayer.grade >= 10) {
-      return res.status(400).json({ message: '최대 강화입니다.' });
+    if (owningPlayer.count < 2) {
+      return res.status(401).json({
+        message: '같은 선수의 카드가 두 장이 있어야 강화할 수 있습니다.',
+      });
     }
 
-    const successProbability = enhancementProbability[owningPlayer.grade + 1];
+    const successProbability = enhancementProbability[grade];
     const random = Math.random();
 
-    if (random < successProbability) {
-      // 강화 성공
+    console.log(random);
+
+    if (owningPlayer.count === 2) {
+      await prisma.owningPlayer.delete({
+        where: {
+          owningPlayerId: owningPlayer.owningPlayerId,
+        },
+      });
+    } else {
       await prisma.owningPlayer.update({
         data: {
-          grade: owningPlayer.grade + 1,
+          count: owningPlayer.count - 2,
         },
         where: {
           owningPlayerId: owningPlayer.owningPlayerId,
         },
       });
-      return res.status(200).json({ message: '강화에 성공하셨습니다!!!' });
+    }
+
+    let updateGrade;
+    let result;
+
+    if (random < successProbability) {
+      // 강화 성공
+      updateGrade = owningPlayer.grade + 1;
+      result = '성공';
     } else {
       // 강화 실패
-      return res.status(200).json({ message: '강화에 실패했습니다.' });
+      if (owningPlayer.grade < 4) {
+        updateGrade = 1;
+      } else if (owningPlayer.grade < 7) {
+        updateGrade = owningPlayer.grade - 3;
+      } else {
+        //7카 이상일 때 일정 확률로 0카
+        const failRandom = Math.random();
+        if (failRandom < explodedProbability) {
+          updateGrade = 0;
+        } else {
+          updateGrade = owningPlayer.grade - 3;
+        }
+      }
+
+      result = '실패';
     }
+
+    const gradePlayer = await prisma.owningPlayer.findFirst({
+      where: {
+        userId,
+        playerId,
+        grade: updateGrade,
+      },
+    });
+
+    if (gradePlayer) {
+      // 이미 +1 카드가 보유 선수에 존재 한다면
+      await prisma.owningPlayer.update({
+        data: {
+          count: gradePlayer.count + 1,
+        },
+        where: {
+          owningPlayerId: gradePlayer.owningPlayerId,
+        },
+      });
+    } else {
+      // +1 카드가 보유 선수에 없다면
+      await prisma.owningPlayer.create({
+        data: {
+          userId,
+          playerId,
+          grade: updateGrade,
+        },
+      });
+    }
+
+    return res.status(200).json({ message: '강화에 ' + result + '했습니다.' });
   } catch (error) {
     next(error);
   }
