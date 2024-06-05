8d9935b3ea8bf4c726899a0c61b06b2705b8dc7c
wjdrbsgkrry <wjdrbsgkrry@naver.com>
2024-06-05 14:27:06 +0900

    테스트중

--- src/app.js
+++ src/app.js
@@ -8,12 +8,20 @@ import userRouter from './routes/user.router.js';
 import owningPlayerRouter from './routes/owning_player.router.js';
 import teamRouter from './routes/team.router.js';
 import matchHistoryRouter from './routes/match-history.router.js';
-
+import tournamentSetting from './routes/tournament.setting.router.js';
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api', [gamePlayRouter, userRouter, owningPlayerRouter, teamRouter, rankingRouter, matchHistoryRouter]);
+app.use('/api', [
+  gamePlayRouter,
+  userRouter,
+  owningPlayerRouter,
+  teamRouter,
+  rankingRouter,
+  matchHistoryRouter,
+  tournamentSetting,
+]);
 app.use(errorHandlerMiddleware);
 
 const PORT = process.env.PORT || 3000;
---
+++ src/routes/tournament.setting.router.js
@@ -0,0 +1,65 @@
+import express from 'express';
+import { prisma } from '../utils/prisma/index.js';
+import authMiddleware from '../middlewares/auth.middleware.js';
+
+const router = express.Router();
+
+router.post(
+  '/tournament/:tournamentId',
+  authMiddleware,
+  async (req, res, next) => {
+    try {
+      const { tournamentId } = req.params;
+
+      const tnmt = await prisma.tournament.findFirst({
+        where: { tournamentId: +tournamentId },
+      });
+
+      const entryDatas = Array.from(
+        await prisma.tournamentEntry.findMany({
+          where: { tournamentId: +tournamentId },
+        }),
+      );
+
+      if (tnmt.winnerTeamId) {
+        return res.status(401).json({ message: '이미 끝난 토너먼트입니다!' });
+      }
+
+      if (Math.floor((new Date() - tnmt.scheduledTime) / 1000 / 60) < 60) {
+        return res
+          .status(404)
+          .json({ message: '아직 시작되지 않은 토너먼트입니다!' });
+      }
+
+      if (entryDatas.length !== 2) {
+        return res
+          .status(404)
+          .json({ message: '현재 토너먼트 유저가 모집되지 않았습니다!' });
+      }
+
+      let teamIds = entryDatas.map((tn) => tn.teamId);
+
+      for (let i = 0; i < 4; i++) {
+        const randomTeam_A = Math.floor(Math.random() * teamIds.length);
+        teamIds.splice(randomTeam_A, 1);
+
+        const randomTeam_B = Math.floor(Math.random() * teamIds.length);
+        teamIds.splice(randomTeam_B, 1);
+
+        await prisma.tournamentMatch.create({
+          data: {
+            tournamentId: +tournamentId,
+            teamAId: randomTeam_A,
+            teamBId: randomTeam_B,
+          },
+        });
+      }
+
+      return res.status(201).json({ message: '대진표가 완성되었습니다!' });
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
+export default router;
