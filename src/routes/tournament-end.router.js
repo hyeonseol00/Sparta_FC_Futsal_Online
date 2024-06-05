35807dea8c860ed9d3962efb5c25d4164ad45216
hyeonseol00 <hyeonseol22@naver.com>
2024-06-05 12:17:18 +0900

    ✨ update : 데이터베이스 스키마 수정에 따른 코드 수정

--- src/routes/tournament-end.router.js
+++ src/routes/tournament-end.router.js
@@ -5,192 +5,179 @@ import authMiddleware from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
-router.post('/tournament/end', async (req, res, next) =>
-{
-	try
-	{
-		const { tournamentId } = req.body;
+router.post('/tournament/end', async (req, res, next) => {
+  try {
+    const { tournamentId } = req.body;
 
-		const tournament = await prisma.tournament.findFirst({ where: { tournamentId } });
-		const tournamentMatch = await prisma.tournament.findFirst({
-			where: {
-				tournamentId
-			},
-			select: {
-				semi1UserATeamId: true,
-				semi1UserBTeamId: true,
-				semi2UserATeamId: true,
-				semi2UserBTeamId: true,
-				finalUserATeamId: true,
-				finalUserBTeamId: true,
-			}
-		});
+    const tournament = await prisma.tournament.findFirst({ where: { tournamentId } });
+    const tournamentSemiMatch = await prisma.tournamentMatch.findMany({
+      where: {
+        tournamentId,
+        roundName: 'semi',
+      },
+    });
+    const tournamentFinalMatch = await prisma.tournamentMatch.findFirst({
+      where: {
+        tournamentId,
+        roundName: 'final',
+      },
+    });
 
-		if (!tournament)
-			return res.status(404).json({ errorMessage: '토너먼트를 찾을 수 없습니다.' });
-		else if (tournament.winnerTeamId == null)
-			return res.status(404).json({ errorMessage: '토너먼트가 아직 진행중입니다!' });
+    if (!tournament) return res.status(404).json({ errorMessage: '토너먼트를 찾을 수 없습니다.' });
+    else if (tournament.winnerTeamId == null)
+      return res.status(404).json({ errorMessage: '토너먼트가 아직 진행중입니다!' });
 
-		// 상품 수여
-		const teamIds = [
-			tournamentMatch.semi1UserATeamId, tournamentMatch.semi1UserBTeamId,
-			tournamentMatch.semi2UserATeamId, tournamentMatch.semi2UserBTeamId
-		];
+    // 상품 수여
+    const teamIds = [
+      tournamentSemiMatch[0].teamAId,
+      tournamentSemiMatch[0].teamBId,
+      tournamentSemiMatch[1].teamAId,
+      tournamentSemiMatch[1].teamBId,
+    ];
 
-		const firstTeamId = tournament.winnerTeamId;
-		const secondTeamId = tournamentMatch.finalUserATeamId == firstTeamId ?
-			tournamentMatch.finalUserBTeamId :
-			tournamentMatch.finalUserATeamId;
-		let thirdTeamAId, thirdTeamBId;
-		for (let i = 0; i < teamIds.length; i++)
-		{
-			if (teamIds[i] == firstTeamId || teamIds[i] == secondTeamId)
-				break;
+    const firstTeamId = tournament.winnerTeamId;
+    const secondTeamId =
+      tournamentFinalMatch.teamAId == firstTeamId
+        ? tournamentFinalMatch.teamBId
+        : tournamentFinalMatch.teamAId;
+    let thirdTeamAId, thirdTeamBId;
+    for (let i = 0; i < teamIds.length; i++) {
+      if (teamIds[i] == firstTeamId || teamIds[i] == secondTeamId) continue;
 
-			if (!thirdUserAId)
-				thirdTeamAId = teamIds[i];
-			else
-				thirdTeamBId = teamIds[i];
-		}
+      if (!thirdTeamAId) thirdTeamAId = teamIds[i];
+      else thirdTeamBId = teamIds[i];
+    }
 
-		const firstTeam = await prisma.team.findFirst({ where: { teamId: firstTeamId } });
-		const secondTeam = await prisma.team.findFirst({ where: { teamId: secondTeamId } });
-		const thirdATeam = await prisma.team.findFirst({ where: { teamId: thirdTeamAId } });
-		const thirdBTeam = await prisma.team.findFirst({ where: { teamId: thirdTeamBId } });
+    const firstTeam = await prisma.team.findFirst({ where: { teamId: firstTeamId } });
+    const secondTeam = await prisma.team.findFirst({ where: { teamId: secondTeamId } });
+    const thirdATeam = await prisma.team.findFirst({ where: { teamId: thirdTeamAId } });
+    const thirdBTeam = await prisma.team.findFirst({ where: { teamId: thirdTeamBId } });
 
-		const firstUserOwningPlayer = await prisma.owningPlayer.findFirst({
-			where: {
-				userId: firstTeam.userId,
-				playerId: tournament.rewardPlayerId
-			}
-		});
-		const secondUserOwningPlayer = await prisma.owningPlayer.findFirst({
-			where: {
-				userId: secondTeam.userId,
-				playerId: tournament.rewardPlayerId
-			}
-		});
-		const thirdUserAOwningPlayer = await prisma.owningPlayer.findFirst({
-			where: {
-				userId: thirdATeam.userId,
-				playerId: tournament.rewardPlayerId
-			}
-		});
-		const thirdUserBOwningPlayer = await prisma.owningPlayer.findFirst({
-			where: {
-				userId: thirdBTeam.userId,
-				playerId: tournament.rewardPlayerId
-			}
-		});
+    const firstUserOwningPlayer = await prisma.owningPlayer.findFirst({
+      where: {
+        userId: firstTeam.userId,
+        playerId: tournament.rewardPlayerId,
+      },
+    });
+    const secondUserOwningPlayer = await prisma.owningPlayer.findFirst({
+      where: {
+        userId: secondTeam.userId,
+        playerId: tournament.rewardPlayerId,
+      },
+    });
+    const thirdUserAOwningPlayer = await prisma.owningPlayer.findFirst({
+      where: {
+        userId: thirdATeam.userId,
+        playerId: tournament.rewardPlayerId,
+      },
+    });
+    const thirdUserBOwningPlayer = await prisma.owningPlayer.findFirst({
+      where: {
+        userId: thirdBTeam.userId,
+        playerId: tournament.rewardPlayerId,
+      },
+    });
 
-		await prisma.$transaction(
-			async (tx) =>
-			{
-				// 1등 보상
-				if (firstUserOwningPlayer)
-				{
-					await tx.owningPlayer.update({
-						data: {
-							count: firstUserOwningPlayer.count + 3,
-						},
-						where: {
-							userId: firstTeam.userId,
-							playerId: tournament.rewardPlayerId
-						}
-					});
-				} else
-				{
-					await tx.owningPlayer.create({
-						data: {
-							count: 3,
-							userId: firstTeam.userId,
-							playerId: tournament.rewardPlayerId
-						},
-					});
-				}
+    await prisma.$transaction(
+      async (tx) => {
+        // 1등 보상
+        if (firstUserOwningPlayer) {
+          await tx.owningPlayer.update({
+            data: {
+              count: firstUserOwningPlayer.count + 3,
+            },
+            where: {
+              owningPlayerId: firstUserOwningPlayer.owningPlayerId,
+            },
+          });
+        } else {
+          await tx.owningPlayer.create({
+            data: {
+              count: 3,
+              userId: firstTeam.userId,
+              playerId: tournament.rewardPlayerId,
+              grade: tournament.rewardPlayerGrade,
+            },
+          });
+        }
 
-				// 2등 보상
-				if (secondUserOwningPlayer)
-				{
-					await tx.owningPlayer.update({
-						data: {
-							count: secondUserOwningPlayer.count + 2,
-						},
-						where: {
-							userId: secondTeam.userId,
-							playerId: tournament.rewardPlayerId
-						}
-					});
-				} else
-				{
-					await tx.owningPlayer.create({
-						data: {
-							count: 2,
-							userId: secondTeam.userId,
-							playerId: tournament.rewardPlayerId
-						},
-					});
-				}
+        // 2등 보상
+        if (secondUserOwningPlayer) {
+          await tx.owningPlayer.update({
+            data: {
+              count: secondUserOwningPlayer.count + 2,
+            },
+            where: {
+              owningPlayerId: secondUserOwningPlayer.owningPlayerId,
+            },
+          });
+        } else {
+          await tx.owningPlayer.create({
+            data: {
+              count: 2,
+              userId: secondTeam.userId,
+              playerId: tournament.rewardPlayerId,
+              grade: tournament.rewardPlayerGrade,
+            },
+          });
+        }
 
-				// 3등 보상
-				if (thirdUserAOwningPlayer)
-				{
-					await tx.owningPlayer.update({
-						data: {
-							count: thirdUserAOwningPlayer.count + 1,
-						},
-						where: {
-							userId: thirdATeam.userId,
-							playerId: tournament.rewardPlayerId
-						}
-					});
-				} else
-				{
-					await tx.owningPlayer.create({
-						data: {
-							count: 1,
-							userId: thirdATeam.userId,
-							playerId: tournament.rewardPlayerId
-						},
-					});
-				}
+        // 3등 보상
+        if (thirdUserAOwningPlayer) {
+          await tx.owningPlayer.update({
+            data: {
+              count: thirdUserAOwningPlayer.count + 1,
+            },
+            where: {
+              owningPlayerId: thirdUserAOwningPlayer.owningPlayerId,
+            },
+          });
+        } else {
+          await tx.owningPlayer.create({
+            data: {
+              count: 1,
+              userId: thirdATeam.userId,
+              playerId: tournament.rewardPlayerId,
+              grade: tournament.rewardPlayerGrade,
+            },
+          });
+        }
 
-				if (thirdUserBOwningPlayer)
-				{
-					await tx.owningPlayer.update({
-						data: {
-							count: thirdUserBOwningPlayer.count + 1,
-						},
-						where: {
-							userId: thirdBTeam.userId,
-							playerId: tournament.rewardPlayerId
-						}
-					});
-				} else
-				{
-					await tx.owningPlayer.create({
-						data: {
-							count: 1,
-							userId: thirdBTeam.userId,
-							playerId: tournament.rewardPlayerId
-						},
-					});
-				}
-			}
-		);
+        if (thirdUserBOwningPlayer) {
+          await tx.owningPlayer.update({
+            data: {
+              count: thirdUserBOwningPlayer.count + 1,
+            },
+            where: {
+              owningPlayerId: thirdUserBOwningPlayer.owningPlayerId,
+            },
+          });
+        } else {
+          await tx.owningPlayer.create({
+            data: {
+              count: 1,
+              userId: thirdBTeam.userId,
+              playerId: tournament.rewardPlayerId,
+              grade: tournament.rewardPlayerGrade,
+            },
+          });
+        }
+      },
+      {
+        isolationLevel: Prisma.TransactionIsolationLevel.ReadCommitted,
+      },
+    );
 
-		return res.status(201).json({
-			message: "토너먼트 종료 처리 완료!",
-			first: firstTeam.userId,
-			second: secondTeam.userId,
-			thirdA: thirdATeam.userId,
-			thirdB: thirdBTeam.userId,
-		});
-	}
-	catch (error)
-	{
-		next(error);
-	}
+    return res.status(201).json({
+      message: '토너먼트 종료 처리 완료!',
+      first: firstTeam.userId,
+      second: secondTeam.userId,
+      thirdA: thirdATeam.userId,
+      thirdB: thirdBTeam.userId,
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 export default router;
